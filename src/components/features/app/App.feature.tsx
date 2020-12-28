2fc6910842b110b6f4aa3fd56704c5c515c514f5
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 15:17:26 +0300

    Feat: add AppFeature

---
+++ src/components/features/app/App.feature.tsx
@@ -0,0 +1,9 @@
+const AppFeature = () => {
+  return (
+    <div>
+      <p>App</p>
+    </div>
+  );
+};
+
+export default AppFeature;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import AppFeature from './components/features/app/App.feature';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppFeature />
   </React.StrictMode>,
   document.getElementById('root')
 );
