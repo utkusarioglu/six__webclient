7b578428b8f98734a23b1a53a327d5667d42fe2f
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-31 19:56:00 +0300

    Feat: add username pass accepted limits as config

--- src/@types/node.d.ts
+++ src/@types/node.d.ts
@@ -1,7 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_SERVER_EXPOSED_PORT: string; // this is a number
+      NODE_SERVER_EXPOSED_PORT: string; // number
+
+      AUTH_USERNAME_LENGTH_MIN: string; // number
+      AUTH_USERNAME_LENGTH_MAX: string; // number
+      AUTH_PASSWORD_LENGTH_MIN: string; // number
+      AUTH_PASSWORD_LENGTH_MAX: string; // number
     }
   }
 }
