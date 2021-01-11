101ff20b40136dc6bdcf4e74ac2d8afcbd48b840
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-11 15:15:36 +0300

    Feat: update posts endpoint

--- src/components/features/app/App.feature.tsx
+++ src/components/features/app/App.feature.tsx
@@ -4,7 +4,7 @@ import { SERVER_PORT, API_ENDPOINT } from '../../../config';
 const AppFeature = () => {
   const [content, setContent] = useState('');
   useEffect(() => {
-    fetch(`${API_ENDPOINT}/auth/user`)
+    fetch(`${API_ENDPOINT}/posts`)
       .then((data) => data.json())
       .then((json) => setContent(JSON.stringify(json, null, 2)));
   }, []);
