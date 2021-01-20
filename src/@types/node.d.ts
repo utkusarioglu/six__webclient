f7c78619557331be3376442d6df57b3a6dc93f89
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-20 18:17:12 +0300

    Chore: define more global types, fix export {}

--- src/@types/node.d.ts
+++ src/@types/node.d.ts
@@ -9,6 +9,21 @@ declare global {
       REACT_APP_AUTH_USERNAME_LENGTH_MAX: string; // number
       REACT_APP_AUTH_PASSWORD_LENGTH_MIN: string; // number
       REACT_APP_AUTH_PASSWORD_LENGTH_MAX: string; // number
+
+      REACT_APP_GA_ID: string;
     }
   }
+
+  /**
+   * Custom properties received from public/index.php
+   * Note that development and production values of these properties are
+   * populated by public/index.php while test values are populated by jest
+   * in src/setupTest.js
+   */
+  interface Window {
+    /** Used by redux */
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: typeof compose;
+  }
 }
+
+export {};
