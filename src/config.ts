54aebbb145ecba3e21063c312716779437b5f7a6
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-20 18:17:37 +0300

    Feat: define more config vars

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
-const { REACT_APP_SERVER_PORT, REACT_APP_SECURE_SCHEMES } = process.env;
+const {
+  REACT_APP_SERVER_PORT,
+  REACT_APP_SECURE_SCHEMES,
+  REACT_APP_GA_ID,
+} = process.env;
 
 export const SERVER_PORT = +(REACT_APP_SERVER_PORT || 8080);
 export const SECURE_SCHEMES =
   REACT_APP_SECURE_SCHEMES !== 'FALSE' ? true : false;
 
 export const HTTP_SCHEME = SECURE_SCHEMES ? 'https' : 'http';
-
 export const API_ENDPOINT = `${HTTP_SCHEME}://${window.location.host}/api`;
+export const GA_ID = REACT_APP_GA_ID;
