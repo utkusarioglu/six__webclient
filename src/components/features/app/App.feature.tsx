77e5916ec8fb30eb417d156a27a65b01342c4b42
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-22 09:48:14 +0300

    Feat: add routes to App.feature

--- src/components/features/app/App.feature.tsx
+++ src/components/features/app/App.feature.tsx
@@ -1,19 +1,47 @@
-import { useState, useEffect } from 'react';
-import { SERVER_PORT, API_ENDPOINT } from '../../../config';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import AppBarView from '_base/components/views/app-bar/AppBar.view';
+import PostsRoute from '_routes/posts/Posts.route';
+import PostDetailsRoute from '_routes/post-details/PostDetails.route';
+import ProfileRoute from '_routes/profile/Profile.route';
+import SettingsRoute from '_routes/settings/Settings.route';
+import CommunityRoute from '_routes/community/Community.route';
 
 const AppFeature = () => {
-  const [content, setContent] = useState('');
-  useEffect(() => {
-    fetch(`${API_ENDPOINT}/posts`)
-      .then((data) => data.json())
-      .then((json) => setContent(JSON.stringify(json, null, 2)));
-  }, []);
-
   return (
-    <div>
-      <span>Server port: {SERVER_PORT}</span>
-      <pre>{content}</pre>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <PostsRoute />
+          </Route>
+
+          <Route path="/r/:communitySlug" exact>
+            <CommunityRoute />
+          </Route>
+
+          <Route path="/r/:communitySlug/:postSlug" exact>
+            <PostDetailsRoute />
+          </Route>
+
+          <Route path="/u/settings" exact>
+            <SettingsRoute />
+          </Route>
+
+          <Route path="/u/:userSlug" exact>
+            <ProfileRoute />
+          </Route>
+
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 };
 
