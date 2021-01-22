71e75ad21535db8cc2f50fae22c660868db205b3
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-22 09:44:38 +0300

    Feat: add domLinkHelper
    * This module and method is used for connecting react-router-dom's link
    with material-ui's component prop in a customizeable way

---
+++ src/components/helpers/dom-link/DomLink.helper.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+import { Link as Domlink } from 'react-router-dom';
+
+// !any
+type DomLinkHelper = (target: string) => FC<any>;
+
+/**
+ * A wrapper for react-router-dom Link component that allows it to be
+ * easily customized for different routes and be consumed by Material-ui's
+ * component prop
+ *
+ * @param target {string} the target url to link to
+ * */
+const domLinkHelper: DomLinkHelper = (target) => (props) => (
+  <Domlink to={target.toLowerCase()} {...props} />
+);
+
+export default domLinkHelper;
