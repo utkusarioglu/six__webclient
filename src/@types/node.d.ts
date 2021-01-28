853156ce34934984cc87321e6c602e4091d268ef
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-28 20:51:29 +0300

    Chore: add doc for explaining dom.window objects

--- src/@types/node.d.ts
+++ src/@types/node.d.ts
@@ -14,12 +14,6 @@ declare global {
     }
   }
 
-  /**
-   * Custom properties received from public/index.php
-   * Note that development and production values of these properties are
-   * populated by public/index.php while test values are populated by jest
-   * in src/setupTest.js
-   */
   interface Window {
     /** Used by redux */
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: typeof compose;
