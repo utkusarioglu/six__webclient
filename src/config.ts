7fb40a6c9cfc85acf536018f3cb067140a7d6538
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-04 10:38:25 +0300

    Feat: add NODE_ENV to config exports

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 const {
+  NODE_ENV: RAW_NODE_ENV,
   REACT_APP_SERVER_PORT,
   REACT_APP_SECURE_SCHEMES,
   REACT_APP_GA_ID,
@@ -11,3 +12,4 @@ export const SECURE_SCHEMES =
 export const HTTP_SCHEME = SECURE_SCHEMES ? 'https' : 'http';
 export const API_ENDPOINT = `${HTTP_SCHEME}://${window.location.host}/api`;
 export const GA_ID = REACT_APP_GA_ID;
+export const NODE_ENV = RAW_NODE_ENV;
