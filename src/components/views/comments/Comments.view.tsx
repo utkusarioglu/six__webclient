8aa954fdae9d4425f1e28c4098ece4bbce7426bd
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-05 11:23:39 +0300

    Feat: add placeholder comments component

--- src/components/views/comments/Comments.view.tsx
+++ src/components/views/comments/Comments.view.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import rest from '_services/rest/rest';
 
 // ! post slug  shoudl come from the store type, cannot be a string
 type CommentsViewProps = {
@@ -6,6 +7,8 @@ type CommentsViewProps = {
 };
 
 const CommentsView: FC<CommentsViewProps> = ({ postSlug }) => {
+  rest.getCommentsByPostSlug(postSlug);
+
   return (
     <>
       <p>Comments</p>
