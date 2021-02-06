6f59dbf74eadd673037e50006a85fb90af255493
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-06 12:22:54 +0300

    Feat: helper expandPost
    * This helper is going to be used by two slices: post, post-repo

---
+++ src/components/helpers/post/expandPost.ts
@@ -0,0 +1,29 @@
+import { PostExpanded } from '../../slices/posts-repo/posts-repo.slice.types';
+import { PostGetRes } from 'six__public-api';
+
+/**
+ * Computes the inferable properties of the post from the raw version
+ * that is received from the server
+ *
+ * @param raw raw post from the server
+ * @returns expanded post object that multiple components use
+ */
+export function expandPost(raw: PostGetRes['res']): PostExpanded {
+  const { communityName, creatorUsername, postSlug } = raw;
+
+  const communitySlug = communityName.toLowerCase();
+  const creatorSlug = creatorUsername.toLowerCase();
+
+  return {
+    ...raw,
+    receivedAt: Date.now(),
+    allowView: true,
+    communitySlug,
+    creatorSlug,
+    communityUrl: `r/${communitySlug}`,
+    communityStylizedUrl: `r/${communityName}`,
+    creatorUrl: `u/${creatorSlug}`,
+    creatorStylizedUrl: `u/${creatorUsername}`,
+    postUrl: `r/${communitySlug}/${postSlug}`,
+  };
+}
