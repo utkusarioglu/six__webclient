13658974dad0034f71935d037a7d7b46f34375f4
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-06 09:05:51 +0300

    Style: update comments input styling

--- src/theme.ts
+++ src/theme.ts
@@ -1,21 +1,35 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import gray from '@material-ui/core/colors/grey';
-import green from '@material-ui/core/colors/green';
+// import gray from '@material-ui/core/colors/grey';
+// import green from '@material-ui/core/colors/green';
 
-export const lightTheme = createMuiTheme({
-  palette: {
-    type: 'light',
-    primary: {
-      main: gray[400],
-    },
-    secondary: {
-      main: green[500],
-    },
-  },
-});
+// export const lightTheme = createMuiTheme({
+//   palette: {
+//     type: 'light',
+//     primary: {
+//       main: gray[400],
+//     },
+//     secondary: {
+//       main: green[500],
+//     },
+//   },
+// });
 
 export const darkTheme = createMuiTheme({
   palette: {
     type: 'dark',
+  },
+  overrides: {
+    MuiInputBase: {
+      root: {
+        borderRadius: '4px',
+      },
+    },
+    MuiFilledInput: {
+      underline: {
+        '&:before': {
+          display: 'none',
+        },
+      },
+    },
   },
 });
