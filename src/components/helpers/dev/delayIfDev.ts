876e34d36428302a258b2d213d5af2c621cb1bb0
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-06 09:35:25 +0300

    Chore: remove console.log from delayIfDev

--- src/components/helpers/dev/delayIfDev.ts
+++ src/components/helpers/dev/delayIfDev.ts
@@ -3,7 +3,6 @@ import { NODE_ENV } from '_base/config';
 export function delayIfDev<T>(callback: () => T, delay: number = 2000): void {
   if (NODE_ENV === 'development') {
     setTimeout(() => {
-      console.log('callback');
       callback();
     }, delay);
   } else {
