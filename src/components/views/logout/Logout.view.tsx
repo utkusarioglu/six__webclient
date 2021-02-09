913a21e547167f35c59c45b4c62a16a7fee78a2b
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-09 08:19:20 +0300

    Feat: Implement logout view
    * Its a barebones view at this point but it handles logout and delayed
    redirec to front page when logout is finalized

---
+++ src/components/views/logout/Logout.view.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import rest from '_services/rest/rest';
+import { getUser } from '_slices/user/user.slice';
+import { useHistory } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+const LogoutView = () => {
+  const history = useHistory();
+  const user = useSelector(getUser);
+
+  useEffect(() => {
+    rest.logout();
+  }, []);
+
+  if (!user.loggedIn) {
+    setTimeout(() => history.push('/'), 1000);
+  }
+
+  return <Typography>Logging out...</Typography>;
+};
+
+export default LogoutView;
