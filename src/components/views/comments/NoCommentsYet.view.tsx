fbd91fc65dc154308b8949b82cc304b3ffcc2ddb
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-25 22:39:01 +0300

    Feat: NoCommentsYetView component

---
+++ src/components/views/comments/NoCommentsYet.view.tsx
@@ -0,0 +1,11 @@
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const NoCommentsYetView = () => (
+  <Container>
+    <Typography>Looks like there are no comments for this post yet.</Typography>
+    <Typography>You can be the first one to say "First!!1!"</Typography>
+  </Container>
+);
+
+export default NoCommentsYetView;
