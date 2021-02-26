d8b355792377d9f55a15f47cc4bf64c3cf864633
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-26 15:15:14 +0300

    Feat: add type for comment save body

--- src/components/slices/comments/comments.slice.types.ts
+++ src/components/slices/comments/comments.slice.types.ts
@@ -1,8 +1,11 @@
-import type { PostEndpoint } from 'six__public-api';
+import type { PostEndpoint, CommentEndpoint } from 'six__public-api';
 import { AsSkeleton } from '_types/material-ui';
 
 export type Comment = PostEndpoint['_comments']['_v1']['_get']['_res']['Success']['body'][0];
-
+export type CommentSaveBody = CommentEndpoint['_save']['_v1']['_post']['_req']['Body'] & {
+  creatorUsername: string;
+  postSlug: string;
+};
 /**
  * Shape of the comment object in the comments store
  */
