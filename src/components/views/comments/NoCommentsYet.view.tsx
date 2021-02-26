0a9aac37542d7b2ee46312b01cb50875daca4585
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-26 10:20:56 +0300

    Update: design of NoCommentsYetView

--- src/components/views/comments/NoCommentsYet.view.tsx
+++ src/components/views/comments/NoCommentsYet.view.tsx
@@ -1,11 +1,31 @@
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import SentimentVeryDissatisfiedRounded from '@material-ui/icons/SentimentVeryDissatisfiedRounded';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 
-const NoCommentsYetView = () => (
-  <Container>
-    <Typography>Looks like there are no comments for this post yet.</Typography>
-    <Typography>You can be the first one to say "First!!1!"</Typography>
-  </Container>
+const NoCommentsYetView = () => {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Grid container justify="center">
+        <SentimentVeryDissatisfiedRounded className={classes.icon} />
+        <Typography>There are no comments for this post yet</Typography>
+        <Typography>You can be the first one to say "First!!1!"</Typography>
+      </Grid>
+    </Container>
+  );
+};
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    icon: {
+      padding: theme.spacing(3),
+      height: 100,
+      width: 100,
+      color: theme.palette.secondary.light,
+    },
+  })
 );
 
 export default NoCommentsYetView;
