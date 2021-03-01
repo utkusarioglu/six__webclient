ba0bd02f711468f7321ddd9e624fa3cc4abed3c1
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-01 09:23:58 +0300

    Feat: add startup actions for snacks
    * Startup snacks and their edge cases defined in startup

--- src/startup.ts
+++ src/startup.ts
@@ -1,6 +1,10 @@
+import { pushSnack } from '_slices/snacks/snacks.slice';
+import cookies from '_services/cookies/cookies';
 import ReactGa from 'react-ga';
 import { GA_ID } from '_config';
 import rest from '_services/rest/rest';
+import { getState } from '_store/store';
+
 /**
  * Handles events that need to happen at startup
  */
@@ -9,4 +13,27 @@ export default async function startup() {
 
   await rest.getSession();
 
+  if (!cookies.getCookieConsent()) {
+    // Cookie prompt links to login prompt through actions,
+    // don't list login prompt separately
+    pushSnack('cookiePrompt');
+  } else if (
+    getState().user.state === 'visitor' &&
+    !isAtLoginPromptDisabledPath()
+  ) {
+    pushSnack('loginPrompt');
+  }
+}
+
+function isAtLoginPromptDisabledPath(): boolean {
+  switch (window.location.pathname.slice(1)) {
+    case 'login':
+    case 'signup':
+    case 'profile':
+    case 'settings':
+      return true;
+
+    default:
+      return false;
+  }
 }
