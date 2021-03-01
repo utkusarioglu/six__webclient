84b3c1e4b39dec78d74b6db6ba09d8297f5558d8
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-01 16:37:13 +0300

    Feat: add expandCommunity helper
    * Soon community data will have a separate store slice for displaying
    single community content, community repo and community display will
     have common properties that will require expansion, this helper allows
     that.

---
+++ src/slices/@shared/expandCommunity.ts
@@ -0,0 +1,20 @@
+import type { CommunityEndpoint_single_res_body } from '_types/public-api';
+import type { CommunityState } from '_slices/community/community.slice.types';
+
+/**
+ * Computes the inferable properties of the community from the raw version
+ * that is received from the server
+ *
+ * @param raw raw community from the server
+ * @returns expanded community object that multiple components use
+ */
+export function expandCommunity(
+  raw: CommunityEndpoint_single_res_body
+): CommunityState {
+  const { slug } = raw;
+
+  return {
+    ...raw,
+    communityUrl: `r/${slug}`,
+  };
+}
