0e6e017a32eb105eb97bd61d1c0b53a886d0762e
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-13 14:52:45 +0300

    Update: Public api overhaul reflections
    * This commit reflects the overhaul of the public api

--- src/@types/public-api.ts
+++ src/@types/public-api.ts
@@ -1,68 +1,75 @@
 import {
-  PostEndpoint,
-  UserEndpoint,
-  CommunityEndpoint,
-  CommentEndpoint,
+  UserEp,
+  CommunityEp,
+  CommentEp,
+  PostEp,
   CommunityActionTypes,
   UserLoginResSuccessful,
+  VoteTypes,
 } from 'six__public-api';
 
 /**
  * Post
  */
-export type PostEndpoint_single = PostEndpoint['_single']['_v1'];
-export type PostEndpoint_single_res_body = PostEndpoint_single['_get']['_res']['Success']['body'];
-export type PostEndpoint_single_res_body_slug = PostEndpoint_single_res_body['postSlug'];
-export type PostEndpoint_single_res_body_id = PostEndpoint_single_res_body['id'];
+export type PostEp_single = PostEp['_single']['_v1'];
+export type PostEp_single_res_body = PostEp_single['_get']['_res']['Success']['body'];
+export type PostEp_single_res_body_slug = PostEp_single_res_body['postSlug'];
+export type PostEp_single_res_body_id = PostEp_single_res_body['id'];
 
-export type PostEndpoint_list = PostEndpoint['_list']['_v1'];
-export type PostEndpoint_list_res_body = PostEndpoint_list['_get']['_res']['Success']['body'];
+export type PostEp_list = PostEp['_list']['_v1'];
+export type PostEp_list_res_body = PostEp_list['_get']['_res']['Success']['body'];
 
-export type PostEndpoint_comments = PostEndpoint['_comments']['_v1'];
+export type PostEp_comments = PostEp['_comments']['_v1'];
 
-export type PostEndpoint_comments_res_body = PostEndpoint_comments['_get']['_res']['Success']['body'];
-export type PostEndpoint_comment_res_body = PostEndpoint_comments['_get']['_res']['Success']['body'][0];
+export type PostEp_comments_res_body = PostEp_comments['_get']['_res']['Success']['body'];
+export type PostEp_comment_res_body = PostEp_comments['_get']['_res']['Success']['body'][0];
+
+export type PostEp_vote = PostEp['_vote']['_v1'];
+export type PostEp_vote_req_body = PostEp_vote['_post']['_req']['Body'];
+export type PostEp_vote_req_params = PostEp_vote['_post']['_req']['Body'];
+
+export type { VoteTypes };
 
 /**
  * User community subscription
  */
-export type UserEndpoint_ucs_alter = UserEndpoint['_user_community_subscription']['_alter']['_v1'];
-export type UserEndpoint_ucs_alter_req_params_actionType = UserEndpoint_ucs_alter['_post']['_req']['Params']['actionType'];
+export type UserEndpoint_ucs_alter = UserEp['_user_community_subscription']['_ucs']['_v1'];
+export type UserEndpoint_ucs_alter_req_params_actionType = UserEndpoint_ucs_alter['_post']['_req']['Body']['actionType'];
 
 /**
  * Signup, login, logout
  */
-export type UserEndpoint_logout = UserEndpoint['_logout']['_v1'];
+export type UserEp_logout = UserEp['_logout']['_v1'];
 
-export type UserEndpoint_login = UserEndpoint['_login']['_v1'];
-export type UserEndpoint_login_req_body = UserEndpoint_login['_post']['_req']['Body'];
+export type UserEp_login = UserEp['_login']['_v1'];
+export type UserEp_login_req_body = UserEp_login['_post']['_req']['Body'];
 
-export type UserEndpoint_signup = UserEndpoint['_signup']['_v1'];
-export type UserEndpoint_signup_req_body = UserEndpoint_signup['_post']['_req']['Body'];
+export type UserEp_signup = UserEp['_signup']['_v1'];
+export type UserEp_signup_req_body = UserEp_signup['_post']['_req']['Body'];
 
-export type UserEndpoint_session = UserEndpoint['_session']['_v1'];
-export type UserEndpoint_session_res_body = UserEndpoint_session['_get']['_res']['Success']['body'];
+export type UserEp_session = UserEp['_session']['_v1'];
+export type UserEp_session_res_body = UserEp_session['_get']['_res']['Success']['body'];
 
-export type UserEndpoint_session_res_body_success = UserLoginResSuccessful;
-export type UserEndpoint_session_res_body_success_username = UserLoginResSuccessful['username'];
-export type UserEndpoint_session_res_body_success_id = UserLoginResSuccessful['id'];
+export type UserEp_session_res_body_success = UserLoginResSuccessful;
+export type UserEp_session_res_body_success_username = UserLoginResSuccessful['username'];
+export type UserEp_session_res_body_success_id = UserLoginResSuccessful['id'];
 
-export type UserEndpoint_ucs_id_list = UserEndpoint['_user_community_subscription']['_id_list']['_v1'];
+export type UserEp_ucs_id_list = UserEp['_user_community_subscription']['_id_list']['_v1'];
 
 /**
  * Community
  */
-export type CommunityEndpoint_list = CommunityEndpoint['_list']['_v1'];
-export type CommunityEndpoint_list_res_body = CommunityEndpoint_list['_get']['_res']['Success']['body'];
+export type CommunityEp_list = CommunityEp['_list']['_v1'];
+export type CommunityEp_list_res_body = CommunityEp_list['_get']['_res']['Success']['body'];
 
-export type CommunityEndpoint_single = CommunityEndpoint['_single']['_v1'];
-export type CommunityEndpoint_single_res_body = CommunityEndpoint_single['_get']['_res']['Success']['body'];
-export type CommunityEndpoint_single_res_body_id = CommunityEndpoint_single_res_body['id'];
+export type CommunityEp_single = CommunityEp['_single']['_v1'];
+export type CommunityEp_single_res_body = CommunityEp_single['_get']['_res']['Success']['body'];
+export type CommunityEp_single_res_body_id = CommunityEp_single_res_body['id'];
 
 export type { CommunityActionTypes };
 
 /**
  * Comment
  */
-export type CommentEndpoint_save = CommentEndpoint['_save']['_v1'];
-export type CommentEndpoint_save_req_body = CommentEndpoint_save['_post']['_req']['Body'];
+export type CommentEp_save = CommentEp['_save']['_v1'];
+export type CommentEp_save_req_body = CommentEp_save['_post']['_req']['Body'];
