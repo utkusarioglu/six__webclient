dd966f198716cc20426d5cd21d275b62d8e96b76
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-15 10:34:01 +0300

    Feat: PostFeed.view feedType, cardType
    * PostFeed received the visual components for feed types and card types
    
    * a small padding has been added at the end of the list to make the
    feed more visually pleasing

--- src/components/views/post-feed/PostFeed.view.tsx
+++ src/components/views/post-feed/PostFeed.view.tsx
@@ -1,4 +1,5 @@
-import type { FC } from 'react';
+import type { FC, ChangeEvent } from 'react';
+import { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import { useSelector } from 'react-redux';
 import PostCardView from '_views/post-card/PostCard.view';
@@ -7,13 +8,23 @@ import { selectPostRepo } from '_slices/post-repo/posts-repo.slice';
 import { emptyPost } from '_slices/post/post.slice.constants';
 import { delayIfDev } from '_helpers/dev/delayIfDev';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import domLinkHelper from '_helpers/dom-link/DomLink.helper';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormatAlignJustifyIcon from '@material-ui/icons/FormatAlignJustify';
+import ViewStreamIcon from '@material-ui/icons/ViewStream';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+type SelectOnChange = (e: ChangeEvent<{ value: unknown }>) => void;
 
 const PostFeedView: FC<{}> = () => {
+  const classes = useStyles();
   const { updatedAt, list: posts } = useSelector(selectPostRepo);
   const getPosts = () => delayIfDev(() => rest.getPosts());
-
+  const [feedType, setFeedType] = useState('best');
+  const [cardType, setCardType] = useState('comfy');
   // This is faulty logic
   if (!updatedAt) {
     getPosts();
@@ -27,13 +38,43 @@ const PostFeedView: FC<{}> = () => {
     return <FollowMoreCommunitiesView />;
   }
 
+  const selectFeedTypeOnChange: SelectOnChange = (e) => {
+    const value = e.target.value as string;
+    setFeedType(value);
+  };
+
+  const selectCardTypeOnChange: SelectOnChange = (e) => {
+    const value = e.target.value as string;
+    setCardType(value);
+  };
+
   return (
-    <div>
-      {posts &&
-        posts.map((post) => (
-          <PostCardView {...{ key: post.id, asSkeleton: false, ...post }} />
-        ))}
-    </div>
+    <>
+      <Container>
+        <Grid container className={classes.postFeedControls}>
+          <Select value={feedType} onChange={selectFeedTypeOnChange}>
+            <MenuItem value="best">Best</MenuItem>
+            <MenuItem value="new">New</MenuItem>
+            <MenuItem value="top">Top</MenuItem>
+          </Select>
+
+          <Select value={cardType} onChange={selectCardTypeOnChange}>
+            <MenuItem value="comfy">
+              <ViewStreamIcon />
+            </MenuItem>
+            <MenuItem value="compact">
+              <FormatAlignJustifyIcon />
+            </MenuItem>
+          </Select>
+        </Grid>
+      </Container>
+      <Container disableGutters className={classes.postCardContainer}>
+        {posts &&
+          posts.map((post) => (
+            <PostCardView {...{ key: post.id, asSkeleton: false, ...post }} />
+          ))}
+      </Container>
+    </>
   );
 };
 
@@ -65,4 +106,15 @@ const FollowMoreCommunitiesView = () => (
       <Link component={domLinkHelper('/communities')}>Click here</Link>
     </Typography>
   </Container>
+);
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    postFeedControls: {
+      justifyContent: 'space-between',
+    },
+    postCardContainer: {
+      paddingBottom: theme.spacing(),
+    },
+  })
 );
