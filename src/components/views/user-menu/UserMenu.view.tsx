3db13a07edd8355c2e0a52cf867054c2c7568bad
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-16 17:01:36 +0300

    Update: UserMenu.view
    * The component has been updated to feature the current featureset of
    the app

--- src/components/views/user-menu/UserMenu.view.tsx
+++ src/components/views/user-menu/UserMenu.view.tsx
@@ -2,25 +2,34 @@ import type { MouseEvent, KeyboardEvent } from 'react';
 import { useState } from 'react';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
+import SettingsIcon from '@material-ui/icons/Settings';
+import BubbleChartIcon from '@material-ui/icons/BubbleChart';
 import IconButton from '@material-ui/core/IconButton';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import Container from '@material-ui/core/Container';
 import ListItem from '@material-ui/core/ListItem';
+import PostAddIcon from '@material-ui/icons/PostAdd';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import { selectLoggedIn } from '_slices/user/user.slice';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import AppsIcon from '@material-ui/icons/Apps';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 import { useSelector } from 'react-redux';
 import domLinkHelper from '_helpers/dom-link/DomLink.helper';
+import { selectUser } from '_slices/user/user.slice';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 
 const RightDrawerMenuView = () => {
   const classes = useStyles();
+  const user = useSelector(selectUser);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const isLoggedIn = useSelector(selectLoggedIn);
+  const ProfileLink = domLinkHelper('/profile');
 
   const toggleDrawer = (open: boolean) => (
     event: KeyboardEvent | MouseEvent
@@ -44,46 +53,79 @@ const RightDrawerMenuView = () => {
       onClick={toggleDrawer(false)}
       onKeyDown={toggleDrawer(false)}
     >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      {user.state === 'logged-in' ? (
+        <Container
+          className={classes.userCardContainer}
+          component={ProfileLink}
+        >
+          <Avatar className={classes.avatar} />
+          <Typography align="center" variant="h5" color="textPrimary">
+            {user.username}
+          </Typography>
+          <Typography align="center" variant="body2" color="textPrimary">
+            {user.userStylizedUrl}
+          </Typography>
+        </Container>
+      ) : (
+        <Container className={classes.userCardContainer}>
+          <Avatar className={classes.avatar} />
+          <Typography align="center" variant="h5">
+            visitor
+          </Typography>
+        </Container>
+      )}
+
+      <Divider />
 
       <List>
-        <ListItem button component={domLinkHelper('/communities')}>
+        <ListItem button component={domLinkHelper('/post/create')}>
           <ListItemIcon>
-            <InboxIcon />
+            <PostAddIcon />
           </ListItemIcon>
-          <ListItemText primary="Communities" />
+          <ListItemText primary="Create a Post" />
         </ListItem>
       </List>
 
       <Divider />
 
       <List>
-        {isLoggedIn ? (
+        <ListItem button component={domLinkHelper('/communities')}>
+          <ListItemIcon>
+            <AppsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Communities" />
+        </ListItem>
+
+        <ListItem button component={domLinkHelper('/community/create')}>
+          <ListItemIcon>
+            <AddCircleOutlineIcon />
+          </ListItemIcon>
+          <ListItemText primary="Create a community" />
+        </ListItem>
+      </List>
+
+      <Divider />
+
+      <List>
+        {user.state === 'logged-in' ? (
           <>
-            <ListItem button component={domLinkHelper('/profile')}>
+            <ListItem button component={ProfileLink}>
               <ListItemIcon>
-                <InboxIcon />
+                <AccountCircle />
               </ListItemIcon>
               <ListItemText primary="Profile" />
             </ListItem>
+
             <ListItem button component={domLinkHelper('/settings')}>
               <ListItemIcon>
-                <InboxIcon />
+                <SettingsIcon />
               </ListItemIcon>
               <ListItemText primary="Settings" />
             </ListItem>
+
             <ListItem button component={domLinkHelper('/logout')}>
               <ListItemIcon>
-                <InboxIcon />
+                <DirectionsRunIcon />
               </ListItemIcon>
               <ListItemText primary="Logout" />
             </ListItem>
@@ -92,13 +134,13 @@ const RightDrawerMenuView = () => {
           <>
             <ListItem button component={domLinkHelper('/signup')}>
               <ListItemIcon>
-                <InboxIcon />
+                <EmojiPeopleIcon />
               </ListItemIcon>
               <ListItemText primary="Sign up" />
             </ListItem>
             <ListItem button component={domLinkHelper('/login')}>
               <ListItemIcon>
-                <InboxIcon />
+                <BubbleChartIcon />
               </ListItemIcon>
               <ListItemText primary="Login" />
             </ListItem>
@@ -120,7 +162,7 @@ const RightDrawerMenuView = () => {
         color="inherit"
         onClick={toggleDrawer(true)}
       >
-        {isLoggedIn ? <AccountCircle /> : <MenuIcon />}
+        {user.state === 'logged-in' ? <AccountCircle /> : <MenuIcon />}
       </IconButton>
       <SwipeableDrawer
         anchor={anchor}
@@ -139,9 +181,18 @@ const useStyles = makeStyles((theme) =>
     list: {
       width: 250,
     },
-    // fullList: {
-    //   width: 'auto',
-    // },
+    avatar: {
+      height: 100,
+      width: 100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginBottom: theme.spacing(),
+    },
+    userCardContainer: {
+      marginTop: 30,
+      marginBottom: 30,
+      textDecoration: 'none',
+    },
   })
 );
 
