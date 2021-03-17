278e6a5a7163769241ccddbe9288fac49febdbf5
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-17 09:58:42 +0300

    Feat: CommunityFeed card selection
    * CommunityFeed component now allows card type to be selected
    
    * A new control ribbon above the feed has the selector for the card type
    
    * Same row also now has a title for the feed items listed. For now the
    title reads Communities but in the future,
    this shall be changed with My communities when the list only includes
    user's communities

--- src/components/views/community-feed/CommunityFeed.view.tsx
+++ src/components/views/community-feed/CommunityFeed.view.tsx
@@ -1,17 +1,28 @@
+import type { FC, ChangeEvent } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectCommunities } from '_slices/community-repo/community-repo.slice';
 import { emptyCommunity } from '_slices/community-repo/community-repo.slice.constants';
 import rest from '_services/rest/rest';
 import { delayIfDev } from '_helpers/dev/delayIfDev';
-import CommunityCardComfyView from '../community-card-comfy/CommunityCardComfy.view';
+import CommunityCardComfyView from '_views/community-card-comfy/CommunityCardComfy.view';
+import CommunityCardCompactView from '_views/community-card-compact/CommunityCardCompact.view';
+import Container from '@material-ui/core/Container';
+import CardDensitySelectView from '_views/card-density-select/CardDensitySelect.view';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 import { NoCommunitiesView } from './NoCommunities.view';
 
-const CommunityFeedView = () => {
+type CommunityCardTypes = 'comfy' | 'compact';
+
+export type SelectOnChange = (e: ChangeEvent<{ value: unknown }>) => void;
+
+const CommunityFeedView: FC<{}> = () => {
   const { updatedAt: communitiesUpdatedAt, list: communities } = useSelector(
     selectCommunities
   );
-
+  const [cardType, setCardType] = useState<CommunityCardTypes>('compact');
   const retrieveCommunities = () => delayIfDev(() => rest.getCommunities());
 
   if (!communitiesUpdatedAt) {
@@ -21,26 +32,50 @@ const CommunityFeedView = () => {
   }
 
   if (!communitiesUpdatedAt) {
-    return skeletons();
+    return CommunityCardComfySkeletons();
   }
 
   if (!!communitiesUpdatedAt && !communities.length) {
     return <NoCommunitiesView />;
   }
 
+  const selectCardTypeOnChange: SelectOnChange = (e) => {
+    const value = e.target.value as CommunityCardTypes;
+    setCardType(value);
+  };
+
   return (
     <>
-      {communities &&
-        communities.map((community) => (
-          <CommunityCardComfyView
-            {...{ key: community.id, asSkeleton: false, ...community }}
+      <Container>
+        <Grid container justify="space-between" alignItems="center">
+          <Typography variant="h6">Communities</Typography>
+          <CardDensitySelectView
+            cardType={cardType}
+            onChange={selectCardTypeOnChange}
           />
-        ))}
+        </Grid>
+      </Container>
+      <Container disableGutters>
+        {communities &&
+          cardType === 'comfy' &&
+          communities.map((community) => (
+            <CommunityCardComfyView
+              {...{ key: community.id, asSkeleton: false, ...community }}
+            />
+          ))}
+        {communities &&
+          cardType === 'compact' &&
+          communities.map((community) => (
+            <CommunityCardCompactView
+              {...{ key: community.id, asSkeleton: false, ...community }}
+            />
+          ))}
+      </Container>
     </>
   );
 };
 
-function skeletons() {
+function CommunityCardComfySkeletons() {
   return (
     <>
       {Array(3)
