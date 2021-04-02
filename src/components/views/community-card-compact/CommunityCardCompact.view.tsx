1df7fe939836ccf52330edf7423aa7ffcf08c177
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-02 11:23:51 +0300

    Chore: remove unused import

--- src/components/views/community-card-compact/CommunityCardCompact.view.tsx
+++ src/components/views/community-card-compact/CommunityCardCompact.view.tsx
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Skeleton from '@material-ui/lab/Skeleton';
